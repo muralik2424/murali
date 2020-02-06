250281acbed7b5eb405b026d5ec90e48a684d82b
muralik2424 <murali0373@gmail.com>
2020-02-06 10:42:11 +0530

    first in git

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BlogpostModule } from './blogpost/blogpost.module';
+import { CmspageModule } from './cmspage/cmspage.module';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { BannerComponent } from './banner/banner.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    BannerComponent,
+    PageNotFoundComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CmspageModule,
+    BlogpostModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/blogpost/blogpost.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { BlogpostRoutingModule } from './blogpost-routing.module';
+import { BlogpostFeaturedComponent } from './blogpost-featured/blogpost-featured.component';
+import { BlogpostListComponent } from './blogpost-list/blogpost-list.component';
+import { BlogpostDetailComponent } from './blogpost-detail/blogpost-detail.component';
+import { BlogpostRecentComponent } from './blogpost-recent/blogpost-recent.component';
+import { BlogpostCategoriesComponent } from './blogpost-categories/blogpost-categories.component';
+
+
+@NgModule({
+  declarations: [BlogpostFeaturedComponent, BlogpostListComponent, BlogpostDetailComponent, BlogpostRecentComponent, BlogpostCategoriesComponent],
+  imports: [
+    CommonModule,
+    BlogpostRoutingModule
+  ],
+  exports: [
+    BlogpostFeaturedComponent
+  ],
+})
+export class BlogpostModule { }
